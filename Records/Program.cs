ce76ce8792fbb52e59516469f9d2d97742c5be31
Pratistha Thapa <thapapratistha2@gmail.com>
2023-03-11 21:14:34 -0500

    Plants list

---
+++ Records/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+Console.WriteLine("How many records do you want to add? ");
+var numberOfRecords = int.Parse(Console.ReadLine());
+
+var recordList = new List<Flower>();
+for (int i = 0; i < numberOfRecords; i++)
+{
+  
+    var flower = new Flower();
+
+    Console.WriteLine("Enter the flower's name: ");
+    flower.Name = Console.ReadLine();
+
+    Console.WriteLine("Enter the flower's color: ");
+    flower.Color = Console.ReadLine();
+
+    recordList.Add(flower);
+}
+
+Console.WriteLine("List of flowers:");
+foreach (var flower in recordList)
+{
+    Console.WriteLine($"Name: {flower.Name}, Color: {flower.Color}");
+}
+
+public class Plant
+    {
+        public string Name { get; set; }
+    }
+public class Flower : Plant
+    {
+        public string Color { get; set; }
+    }
+
